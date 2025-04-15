d8a3496283f250c8d45e453a1e0e168556247d6f
halap <halaphin6@gmai.com>
2025-04-15 16:48:19 +0700

    Add some lib and framework

--- src/components/ServiceList.jsx
+++ src/components/ServiceList.jsx
@@ -1,15 +1,35 @@
-import { Service } from "../models/Service";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function ServiceList() {
-  const services = [
-    new Service(1, "Trang trí tiệc cưới", 5000000, "Trang trí"),
-    new Service(2, "Catering hội nghị", 10000000, "Catering"),
-  ];
+  const [services, setServices] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  
+    useEffect(() => {
+    setLoading(true);
+    axios.get("http://localhost:3000/services").then((response) => {
+      setServices(response.data);
+      setLoading(false);
+    });
+  }, []);
+    useEffect(() => {
+    axios
+      .get("http://localhost:3000/services")
+      .then((response) => {
+        setServices(response.data);
+      })
+      .catch((error) => {
+        console.error("Lỗi khi lấy dịch vụ:", error);
+      });
+  }, []);
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-6">
       {services.map((s) => (
         <div key={s.id} className="border rounded p-4 shadow">
           <h2 className="text-xl font-semibold">{s.name}</h2>
-          <p>{s.formatPrice()}</p>
+          <p>{s.price.toLocaleString()} VND</p>
           <p>{s.category}</p>
         </div>
       ))}
